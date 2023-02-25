051f95c5a25957ea4ca6ecf421b929b23c0fe3ff
horiz4n <h0riz4nfeed@mail.ru>
2023-02-25 11:30:22 +0300

    add_db

--- database/database.py
+++ database/database.py
@@ -1,11 +1,12 @@
+# Класс для работы с Базой Данных
+
 class Database:
 
     def __init__():
         pass
 
-    def db_connect():
+    def db_connect(): 
         print("DB connected")
 
     def db_disconnect():
         print("DB disconnected")
-        
